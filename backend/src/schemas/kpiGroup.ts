524d317f4cf66a1a6f9b869dcba6744dbc7939ac
YoungjaeKim <youngjaekim@linecorp.com>
2025-05-15 07:05:46 +0900

    refactor: update server start script and enhance build process with clean step

--- backend/src/schemas/kpiGroup.ts
+++ backend/src/schemas/kpiGroup.ts
@@ -2,12 +2,18 @@ import mongoose, { Document, Schema } from 'mongoose';
 
 export interface IKpiGroup extends Document {
     title: string;
+    archived?: boolean;
 }
 
 const kpiGroupSchema = new Schema<IKpiGroup>({
     title: {
         type: String,
         required: true
+    },
+    archived: {
+        type: Boolean,
+        default: false,
+        required: false
     }
 });
 
